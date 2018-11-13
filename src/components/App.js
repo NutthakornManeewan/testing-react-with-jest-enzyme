26b2058d6d160bdac82f7c8b8e45b9f8adfbf41a
Nutthakorn Maneewan <natthakorn.pao@gmail.com>
2018-11-13 15:12:49 +0700

    configure the test environment

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Loot Check</h2>
+        <p>This app build for test with the TDD process!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
